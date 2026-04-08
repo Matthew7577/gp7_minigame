bbee54b4ba8aaec1e5d8a222c908eca880d9d893
LittleBlue7577 <matthew7577@gmail.com>
2026-04-08 15:21:54 +0800

    Add layout and activity files for levels 1 to 10

---
+++ app/src/main/java/com/gpproject/gp7_minigame/Level4Activity.java
@@ -0,0 +1,10 @@
+﻿package com.gpproject.gp7_minigame;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class Level4Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_4);
+    }
+}
