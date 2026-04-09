d8c2b86451fe0560b3c2066eba7282244be6fe1e
LittleBlue7577 <matthew7577@gmail.com>
2026-04-09 11:41:56 +0800

    Refactor button movement logic into MinigameLogic class and simplify Level1Activity

--- app/src/main/java/com/gpproject/gp7_minigame/Level1Activity.java
+++ app/src/main/java/com/gpproject/gp7_minigame/Level1Activity.java
@@ -1,23 +1,15 @@
 package com.gpproject.gp7_minigame;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Handler;
-import android.os.Looper;
-import java.util.Random;
+
 public class Level1Activity extends AppCompatActivity {
     Button btnClickMe;
     Boolean clicked = false;
-    private int deltaX, deltaY;
     ViewGroup root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +19,9 @@ public class Level1Activity extends AppCompatActivity {
 
         btnClickMe = findViewById(R.id.btnClickMe);
         root = findViewById(R.id.move_area);
-        moveButton();
+
+        MinigameLogic.moveButton(root, btnClickMe, root, () -> clicked, 1000);
+
         if (btnClickMe != null) {
             btnClickMe.setOnClickListener(v -> {
                 SharedPreferences prefs = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
@@ -57,33 +51,4 @@ public class Level1Activity extends AppCompatActivity {
             });
         }
     }
-    public void moveButton() {
-        final Handler handler = new Handler();
-        final Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                ViewGroup main = findViewById(R.id.move_area);
-                Random rand = new Random();
-                final int X = rand.nextInt(main.getWidth()-btnClickMe.getWidth()-2);
-                final int Y = rand.nextInt(main.getHeight()-btnClickMe.getHeight()-2);
-                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) btnClickMe.getLayoutParams();
-                deltaX = X - lParams.leftMargin;
-                deltaY = Y - lParams.topMargin;
-                Log.d("Level1Activity", "Moving button to: (" + X + ", " + Y + ") Delta: (" + deltaX + ", " + deltaY + ")");
-                lParams.leftMargin += deltaX;
-                lParams.topMargin += deltaY;
-                btnClickMe.setLayoutParams(lParams);
-                if (clicked){
-                    return;
-                } else {
-                    handler.postDelayed(this, 1000);
-                }
-            }
-        };
-        handler.post(runnable); // Runs on UI thread
-        // Delayed execution (2 seconds)
-        root.invalidate();
-    }
-
-    
 }
---
+++ app/src/main/java/com/gpproject/gp7_minigame/MinigameLogic.java
@@ -0,0 +1,52 @@
+package com.gpproject.gp7_minigame;
+
+import android.os.Handler;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MinigameLogic {
+    public interface GameState {
+        boolean isFinished();
+    }
+
+    public static void moveButton(ViewGroup main, Button btnClickMe, ViewGroup root, GameState gameState, int delayMillis) {
+        final Handler handler = new Handler();
+        Log.d("MinigameLogic", "Start moveButton.");
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (main != null && btnClickMe != null && main.getWidth() > 0 && main.getHeight() > 0) {
+                    Random rand = new Random();
+                    int xBound = main.getWidth() - btnClickMe.getWidth() - 2;
+                    int yBound = main.getHeight() - btnClickMe.getHeight() - 2;
+
+                    if (xBound > 0 && yBound > 0) {
+                        final int X = rand.nextInt(xBound);
+                        final int Y = rand.nextInt(yBound);
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) btnClickMe.getLayoutParams();
+                        int deltaX = X - lParams.leftMargin;
+                        int deltaY = Y - lParams.topMargin;
+                        Log.d("MinigameLogic", "Moving button to: (" + X + ", " + Y + ") Delta: (" + deltaX + ", " + deltaY + ")");
+                        lParams.leftMargin += deltaX;
+                        lParams.topMargin += deltaY;
+                        btnClickMe.setLayoutParams(lParams);
+                    }
+                }
+
+                if (gameState != null && gameState.isFinished()) {
+                    Log.d("MinigameLogic", "Stop moveButton.");
+                } else {
+                    handler.postDelayed(this, delayMillis);
+                }
+            }
+        };
+        handler.post(runnable); // Runs on UI thread
+        if (root != null) {
+            root.invalidate();
+        }
+    }
+}
