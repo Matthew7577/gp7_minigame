0b7b9429c755d3212d761331bf29409145793a46
Hevin987 <24177961a@common.cpce-polyu.edu.hk>
2026-04-10 23:21:44 +0800

    Refactor Level 2 layout, add interactive button and update movement logic

--- app/src/main/java/com/gpproject/gp7_minigame/Level2Activity.java
+++ app/src/main/java/com/gpproject/gp7_minigame/Level2Activity.java
@@ -3,25 +3,34 @@ package com.gpproject.gp7_minigame;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class Level2Activity extends AppCompatActivity {
+    Button btnClickMe;
+    Boolean clicked = false;
+    ViewGroup root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_2);
         PauseMenuHelper.setupPauseButton(this);
 
-        Button btnFinishLevel = findViewById(R.id.btnFinishLevel);
-        if (btnFinishLevel != null) {
-            btnFinishLevel.setOnClickListener(v -> {
+        btnClickMe = findViewById(R.id.btnClickMe);
+        root = findViewById(R.id.move_area);
+
+        MinigameLogic.moveButton(root, btnClickMe, root, () -> clicked, 500);
+
+        if (btnClickMe != null) {
+            btnClickMe.setOnClickListener(v -> {
                 SharedPreferences prefs = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
                 String currentUser = prefs.getString("currentUser", "");
+                clicked = true;
                 if (!currentUser.isEmpty()) {
-                    prefs.edit().putBoolean(currentUser + "_level_2_finished", true).apply();
-                    Toast.makeText(this, "Level 2 Finished!", Toast.LENGTH_SHORT).show();
+                    prefs.edit().putBoolean(currentUser + "_level_1_finished", true).apply();
+                    Toast.makeText(this, "Level 1 Finished!", Toast.LENGTH_SHORT).show();
                     finish(); // Return to SelectLevelActivity
                 } else {
                     Toast.makeText(this, "Not logged in!", Toast.LENGTH_SHORT).show();
