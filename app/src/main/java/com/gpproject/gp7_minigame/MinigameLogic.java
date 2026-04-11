ad8d3893d55b029bddd1c6b07045ddac7a8d7be4
LittleBlue7577 <matthew7577@gmail.com>
2026-04-11 16:49:11 +0800

    Remove unused import for LinearInterpolator in MinigameLogic.java

--- app/src/main/java/com/gpproject/gp7_minigame/MinigameLogic.java
+++ app/src/main/java/com/gpproject/gp7_minigame/MinigameLogic.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
